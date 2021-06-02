fd041c5436ca866278a8525988de2fbd3d10751a
gilotyna1808 <65335164+gilotyna1808@users.noreply.github.com>
2021-06-02 08:46:54 +0200

    Klonowanie

--- src/pl/polsl/zad1/Main.java
+++ src/pl/polsl/zad1/Main.java
@@ -2,6 +2,6 @@ package pl.polsl.zad1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Github! Kloon");
     }
 }
